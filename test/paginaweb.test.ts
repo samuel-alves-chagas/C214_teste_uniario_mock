d76350643d3f8df49f2e56a8b2f70fcb6a160d77
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2024-03-23 20:32:33 -0300

    Adicionando testes de cenário de sucesso

--- test/paginaweb.test.ts
+++ test/paginaweb.test.ts
@@ -21,42 +21,53 @@ describe('Testando página WEB que apresenta os horários de atendimento', ()=> 
     })
 
     it('Validando que o retorno da consulta é um objeto e não a string entregue pelo servidor', ()=> {
-        const dadosAtendimentoChris = horarioAtendimento.buscarAtendimento("Chris Lima")
-        expect(typeof dadosAtendimentoChris).toBe("object")
+        const dadosAtendimento = horarioAtendimento.buscarAtendimento("Chris Lima")
+        expect(typeof dadosAtendimento).toBe("object")
     })
 
     it('Validando que o retorno da consulta possui o campo nomeDoProfessor', ()=> {
-        const dadosAtendimentoChris = horarioAtendimento.buscarAtendimento("Chris Lima")
-        expect(dadosAtendimentoChris).toHaveProperty("nomeDoProfessor")
+        const dadosAtendimento = horarioAtendimento.buscarAtendimento("Chris Lima")
+        expect(dadosAtendimento).toHaveProperty("nomeDoProfessor")
     })
 
     it('Validando que o retorno da consulta possui o campo horarioDeAtendimento', ()=> {
-        const dadosAtendimentoChris = horarioAtendimento.buscarAtendimento("Chris Lima")
-        expect(dadosAtendimentoChris).toHaveProperty("horarioDeAtendimento")
+        const dadosAtendimento = horarioAtendimento.buscarAtendimento("Chris Lima")
+        expect(dadosAtendimento).toHaveProperty("horarioDeAtendimento")
     })
 
     it('Validando que o retorno da consulta possui o campo periodo', ()=> {
-        const dadosAtendimentoChris = horarioAtendimento.buscarAtendimento("Chris Lima")
-        expect(dadosAtendimentoChris).toHaveProperty("periodo")
+        const dadosAtendimento = horarioAtendimento.buscarAtendimento("Chris Lima")
+        expect(dadosAtendimento).toHaveProperty("periodo")
     })
 
     it('Validando que o retorno da consulta possui o campo sala', ()=> {
-        const dadosAtendimentoChris = horarioAtendimento.buscarAtendimento("Chris Lima")
-        expect(dadosAtendimentoChris).toHaveProperty("sala")
+        const dadosAtendimento = horarioAtendimento.buscarAtendimento("Chris Lima")
+        expect(dadosAtendimento).toHaveProperty("sala")
     })
 
     it('Validando que o retorno da consulta possui o campo predio', ()=> {
-        const dadosAtendimentoChris = horarioAtendimento.buscarAtendimento("Chris Lima")
-        expect(dadosAtendimentoChris).toHaveProperty("predio")
+        const dadosAtendimento = horarioAtendimento.buscarAtendimento("Chris Lima")
+        expect(dadosAtendimento).toHaveProperty("predio")
     })
 
     it('Validando que o nome do professor é Renzo', ()=> {
-        const dadosAtendimentoChris = horarioAtendimento.buscarAtendimento("Renzo")
-        expect(dadosAtendimentoChris.nomeDoProfessor).toBe("Renzo")
+        const dadosAtendimento = horarioAtendimento.buscarAtendimento("Renzo")
+        expect(dadosAtendimento.nomeDoProfessor).toBe("Renzo")
     })
 
-    it('Validando que a propriedade horarioDeAtendimento é uma string', ()=> {
-        const dadosAtendimentoChris = horarioAtendimento.buscarAtendimento("Renzo")
-        expect(typeof dadosAtendimentoChris.horarioDeAtendimento).toBe("string")
+    it('Validando que a propriedade sala possui valor 1', ()=> {
+        const dadosAtendimento = horarioAtendimento.buscarAtendimento("Renzo")
+        expect(dadosAtendimento.sala).toBe("1")
+    })
+
+    it('Validando que por a sala ser 1 o prédio também é 1', ()=> {
+        const dadosAtendimento = horarioAtendimento.buscarAtendimento("Renzo")
+        expect(dadosAtendimento.sala).toBe("1")
+        expect(dadosAtendimento.predio[0]).toBe("1")
+    })
+
+    it('Validando que só pode haver um item no array de prédio', ()=> {
+        const dadosAtendimento = horarioAtendimento.buscarAtendimento("Renzo")
+        expect(dadosAtendimento.predio.length).toBe(1)
     })
 })
