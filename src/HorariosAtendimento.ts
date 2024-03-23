ddec438eb7e5dfea28ae5f26440a4b4f03dcf3b9
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2024-03-23 11:21:35 -0300

    Criando sistema falso de busca de horários de atendimento

---
+++ src/HorariosAtendimento.ts
@@ -0,0 +1,7 @@
+export class HorarioAtendimento {
+    private nomeDoProfessor: string = "";
+    private horarioDeAtendimento: string = "";
+    private periodo: string = "";
+    private sala: string = "";
+    private predio: string[] = [];
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log("hello world")
+import { SistemaAtendimento } from "./SistemaAtendimento"
+
+const sistemdeAtendimentoInatel = new SistemaAtendimento
+console.log(sistemdeAtendimentoInatel.buscaHorarioAtendimentoProfessor("Chris Lima"))
